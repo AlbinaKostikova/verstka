8c05bf98889a854176881b8954d5dc9c940019c9
AlbinaKostikova <albucit33@gmail.com>
2025-11-08 19:46:28 +0600

    added event listener

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-const elements = document.querySelectorAll('header')
-elements.forEach(elem => {
-  console.log(elem)
+const contens = document.querySelectorAll('.program-line__content')
+contens.forEach((elem) => {
+  const title = elem.querySelector('.program-line__title')
+  const descr = elem.querySelector('.program-line__descr')
+
+title.addEventListener('click', () => {
+  descr.classList.toggle('active')
+})
 })
